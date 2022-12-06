9fdf871a66719aace416fe0629f3a9663f53f054
Brendan Muldoon <brendan.muldoon@bt.com>
2022-12-06 14:43:26 +0000

    initial commit

---
+++ src/main/java/ai/com/example/aiexample/AiExampleApplication.java
@@ -0,0 +1,13 @@
+package ai.com.example.aiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/com/example/aiexample/AiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.com.example.aiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
