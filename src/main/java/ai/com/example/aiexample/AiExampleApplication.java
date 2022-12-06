d82072a1eda4682d62233b778d02e4d147ce4548
Brendan Muldoon <50370276+brendanmuldoon@users.noreply.github.com>
2022-12-06 14:44:42 +0000

    Update AiExampleApplication.java

--- src/main/java/ai/com/example/aiexample/AiExampleApplication.java
+++ src/main/java/ai/com/example/aiexample/AiExampleApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class AiExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiExampleApplication.class, args);
 	}
+	
+	@GetMapping("/ai-example")
+  	public String greet() {
+    		return "Hello, world!";
+  	}
 
 }
